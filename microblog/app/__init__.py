b7ff6ddeb3c7aca02b84e76c3d7db537eae73097
rg3915 <rg3915@yahoo.com.br>
2019-05-20 00:58:13 -0300

    Chapter 5: User Logins. Close #2

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
 
 from app import routes, models
