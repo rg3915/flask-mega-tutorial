45c6927f8b59f1ab8f1bbe4ddeb5290b2a5f4716
rg3915 <rg3915@yahoo.com.br>
2019-05-26 01:56:37 -0300

    Chapter 12: Dates and Times. Close #9

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 if not app.debug:
