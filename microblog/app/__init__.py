07169baff08802c343c5e06ad7945a04f448abc5
rg3915 <rg3915@yahoo.com.br>
2019-05-26 01:52:23 -0300

    Chapter 11: Facelift. Close #8

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
